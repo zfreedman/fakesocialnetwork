d4320c83189587ced10bba4218c149e088570cb3
Zach Freedman <zfreedman92@gmail.com>
2018-10-09 16:14:56 -0700

    feat: Began creating basic app layout (only HTML/CSS scaffolding)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Navbar from "./components/layout/Navbar";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>my react app</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
